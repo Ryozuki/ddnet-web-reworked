99af90130515ff0a8cebcf1cbb827a79c7221094
Ryozuki <clud6565@gmail.com>
2017-05-18 19:30:22 +0200

    donation bars

--- ddnet-web/constants.js
+++ ddnet-web/constants.js
@@ -3,7 +3,12 @@
 var constants = {
     title: 'DDraceNetwork',
     video: 'https://www.youtube.com/embed/AfvPA4wqOTQ',
-    version: '10.6'
+    version: '10.6',
+    currentYear: 2017,
+    maxDontationCurYear: 374,
+    donatedCurYear: 105,
+    donated: 1540,
+    maxdonation: 2015
 }
 
 module.exports = constants;
--- ddnet-web/routes/index.js
+++ ddnet-web/routes/index.js
@@ -6,13 +6,24 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   var agent = useragent.parse(req.headers['user-agent']);
+
+  var currentDonationPercent = (constants.donatedCurYear / constants.maxDontationCurYear) * 100;
+  var donationPercent = (constants.donated / constants.maxdonation) * 100;
+
   console.log(agent);
   res.render('index', { 
     title: constants.title,
     version: constants.version,
     useragent: agent,
     video_link: constants.video,
-    download_text: "Download DDraceNetwork Client & Server "
+    download_text: "Download DDraceNetwork Client & Server ",
+    maxDontationCurYear: constants.maxDontationCurYear,
+    donatedCurYear: constants.donatedCurYear,
+    donated: constants.donated,
+    maxdonation: constants.maxdonation,
+    currentYear: constants.currentYear,
+    currentDonationPercent: currentDonationPercent,
+    donationPercent: donationPercent
  });
 });
 
